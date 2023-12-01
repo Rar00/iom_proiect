ac46d26a7adde290bc04a8a3c372bd5444d092ff
Rar00 <60270872+Rar00@users.noreply.github.com>
2023-12-01 15:10:15 +0200

    Started the project

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  1 15:06:17 2023
+
+@author: Preoteasa Rares
+"""
+
+#Aici punem codul
+#Link exemplu https://thepythoncode.com/article/make-an-image-editor-in-tkinter-python
+##############Sa stergem linia de deasupra la final
+
